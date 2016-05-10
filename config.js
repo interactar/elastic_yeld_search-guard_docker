e6d4e8dc701e99a6462f21bd14e0d9650f23d847
Javier Ailbirt <jailbirt@gmail.com>
2016-05-10 17:30:28 -0300

    Add interactar docker and juansan configuration override

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+	"port" : 3000,
+	"es_host" : "localhost",
+	"es_port" : "9200",
+	"path" : "/opt/rules/",
+	"index": "rulez",
+	"type" : "rule",
+	"defaults" : {
+		"es_host" : "0.0.0.0",
+		"es_port" : 9200
+	}
+}
