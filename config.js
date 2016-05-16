f725a8a3e18f1b7009721bc6a8755c80910c0c32
root <root@bi.canopus.com.ar>
2016-05-16 01:45:42 -0300

    SMTP RELAY

--- config.js
+++ config.js
@@ -1,12 +1,16 @@
 {
-	"port" : 3000,
-	"es_host" : "elasticsearch_host",
-	"es_port" : "9200",
-	"path" : "/opt/rules/",
-	"index": "rulez",
-	"type" : "rule",
-	"defaults" : {
-		"es_host" : "elasticsearch_host",
-		"es_port" : 9200
-	}
+  "port" : 3000,
+    "es_host" : "elasticsearch_host",
+    "es_port" : "9200",
+    "path" : "/opt/rules/",
+    "index": "rulez",
+    "type" : "rule",
+    "defaults" : {
+      "es_host" : "elasticsearch_host",
+      "es_port" : 9200,
+      "type"    : "frequency",
+      "timestamp_field" : "fecha",
+      "index" : "objetivos",
+      "alert" : ["email"]
+    }
 }
